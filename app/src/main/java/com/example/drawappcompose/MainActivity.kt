402397fed8f233c9322aaaf78f9c522eae1aee2e
datsiii <dasha.tsivina@gmail.com>
2024-04-28 23:06:42 +0300

    add draw

--- app/src/main/java/com/example/drawappcompose/MainActivity.kt
+++ app/src/main/java/com/example/drawappcompose/MainActivity.kt
@@ -3,12 +3,20 @@ package com.example.drawappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.drawappcompose.ui.theme.DrawAppComposeTheme
 
@@ -17,16 +25,45 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DrawAppComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                DrawCanvas()
             }
         }
     }
+}
+
+@Composable
+fun DrawCanvas() {
+    val tempPath = Path()
+    val path = remember {
+        mutableStateOf(Path())
+    }
+
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(true) {
+                detectDragGestures { change, dragAmount ->
+                    tempPath.moveTo(
+                        change.position.x - dragAmount.x,
+                        change.position.y - dragAmount.y
+                    )
+                    tempPath.lineTo(
+                        change.position.x,
+                        change.position.y
+                    )
+                    path.value = Path().apply {
+                        addPath(tempPath)
+                    }
+                }
+            }
+    ){
+        drawPath(
+            path.value,
+            color = Color.Black,
+            style = Stroke(5f)
+        )
+    }
+
 }
 
 @Composable
